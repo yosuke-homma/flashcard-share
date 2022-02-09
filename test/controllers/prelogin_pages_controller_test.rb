de8e5c9f9e456e19d0d8ac56c74e6275e2463fac
yosuke-homma <y.homma0708@gmail.com>
2022-02-09 22:37:43 +0900

    Edit titles of prelogin pages

--- test/controllers/prelogin_pages_controller_test.rb
+++ test/controllers/prelogin_pages_controller_test.rb
@@ -1,18 +1,28 @@
 require "test_helper"
 
 class PreloginPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  #setupメソッドは各テストが実行される直前に実行される
+  def setup 
+    @base_title = "Flashcard Share"
+  end
+
+  
   test "should get root" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get prelogin_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get help" do
     get prelogin_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 end
