4df1c3b56799e51b4a5eee4a335be6432906d484
yosuke-homma <y.homma0708@gmail.com>
2022-02-16 21:31:51 +0900

    Set signup page and login page using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+  }
   root 'prelogin_pages#home'
   get '/about', to: 'prelogin_pages#about'
   get  '/help', to: 'prelogin_pages#help'
-  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
